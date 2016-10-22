88a3a93510bf892ec637386f1ddfa134ade25a5b
ErinJaws <eej26@cornell.edu>
2016-10-22 17:03:10 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,15 +1,22 @@
+/*https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagName
+Select all HTML elements in the webpage.
+*/
 var elements = document.getElementsByTagName('*');
-//https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByTagName
+
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
+//For each element consider all of its childern (node).
+
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
-
+/*//https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+If the child is text
+*/
         if (node.nodeType === 3) {
-            //https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+            
             var text = node.nodeValue;
-            var replacedText = text.replace('ugly', 'pretty');
+            var replacedText = text.replace(/ugly/gi, 'pretty');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
