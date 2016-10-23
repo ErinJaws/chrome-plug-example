838b2c453c8246d589119e69ead769b255cd9cf2
ErinJaws <eej26@cornell.edu>
2016-10-23 15:55:07 -0700

    Update content_2.js

--- content_2.js
+++ content_2.js
@@ -9,16 +9,16 @@ for (var i = 0; i < elements.length; i++) {
 
 //For each element consider all of its childern (node).
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+   // for (var j = 0; j < element.childNodes.length; j++) {
+     //   var node = element.childNodes[j];
 /*
 //https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
 If the child is text then take value of the node.
 */
-        if (node.nodeType === 3) {
+        if (element.nodeType === 3) {
 //https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeValue 
             //.replace(/slut/gi,"person"/).replace(/bitch/gi,"superheroine"/)
-            var text = node.nodeValue;
+            var text = element.nodeValue;
             var replacedText = text.replace(/the/gi,"tha");
 
             if (replacedText !== text) {
@@ -26,7 +26,7 @@ If the child is text then take value of the node.
                 console.log('replaced');
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
-        }
+       // }
     }
 }
 
